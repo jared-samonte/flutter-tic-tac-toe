9d0192ccd511aa6fc08bcbf664c6587cca6275cf
jared-samonte <jared.vidal.samonte@gmail.com>
2023-04-14 18:17:32 -0500

    Updated with tutorial

--- tic_tac_toe/lib/main.dart
+++ tic_tac_toe/lib/main.dart
@@ -1,115 +1,273 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+@override
+Widget build(BuildContext context) {
+	return MaterialApp(
+	home: HomePage(),
+	);
+}
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class HomePage extends StatefulWidget {
+@override
+_HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+bool oTurn = true;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// 1st player is O
+List<String> elementToDisplay = ['', '', '', '', '', '', '', '', ''];
+int oPoints = 0;
+int xPoints = 0;
+int usedBoxes = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+@override
+Widget build(BuildContext context) {
+	return Scaffold(
+	backgroundColor: Colors.purple[800],
+	body: Column(
+		children: <Widget>[
+		Expanded(
+			child: Container(
+			child: Row(
+				mainAxisAlignment: MainAxisAlignment.center,
+				children: <Widget>[
+				Padding(
+					padding: const EdgeInsets.all(30.0),
+					child: Column(
+					mainAxisAlignment: MainAxisAlignment.center,
+					children: <Widget>[
+						Text(
+						'Player X',
+						style: TextStyle(fontSize: 20,
+										fontWeight: FontWeight.bold,
+										color: Colors.yellow),
+						),
+						Text(
+              xPoints.toString(),
+              style: TextStyle(fontSize: 20,color: Colors.yellow),
+						),
+					],
+					),
+				),
+				Padding(
+					padding: const EdgeInsets.all(30.0),
+					child: Column(
+					mainAxisAlignment: MainAxisAlignment.center,
+					children: <Widget>[
+						Text('Player O', style: TextStyle(fontSize: 20,
+														fontWeight: FontWeight.bold,
+														color: Colors.yellow)
+						),
+						Text(
+              oPoints.toString(),
+              style: TextStyle(fontSize: 20,color: Colors.yellow),
+						),
+					],
+					),
+				),
+				],
+			),
+			),
+		),
+		Expanded(
+			flex: 4,
+      child: Container(
+        width: 450,
+        height: 450,
+        child: GridView.builder(
+          itemCount: 9,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3),
+          itemBuilder: (BuildContext context, int index) 
+          {
+          return GestureDetector(
+            onTap: () {
+            _tapped(index);
+            },
+            child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.yellow)),
+            child: Center(
+              child: Text(
+              elementToDisplay[index],
+              style: TextStyle(color: Colors.yellow, fontSize: 35),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ],
+          );
+          }),
+      )
+		),
+		Expanded(
+			child: Container(
+			child: Row(
+			mainAxisAlignment: MainAxisAlignment.center,
+			children: <Widget>[
+				 ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.purple[50],
+          onPrimary: Colors.black
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+				onPressed: _clearScoreBoard,
+				child: Text("Clear Score Board"),
+				),
+			],
+			),
+		))
+		],
+	),
+	);
+}
+
+void _tapped(int index) {
+	setState(() 
+  {
+    if (oTurn && elementToDisplay[index] == '') 
+    {
+      elementToDisplay[index] = 'O';
+      usedBoxes++;
+    } 
+    else if (!oTurn && elementToDisplay[index] == '') 
+    {
+      elementToDisplay[index] = 'X';
+      usedBoxes++;
+    }
+
+    oTurn = !oTurn;
+    _checkWinner();
+	});
+}
+
+void _checkWinner() {
+	
+	// Checking rows
+	if (elementToDisplay[0] == elementToDisplay[1] &&
+		elementToDisplay[0] == elementToDisplay[2] &&
+		elementToDisplay[0] != '') 
+    {
+    _showWinDialog(elementToDisplay[0]);
+    }
+	if (elementToDisplay[3] == elementToDisplay[4] &&
+		elementToDisplay[3] == elementToDisplay[5] &&
+		elementToDisplay[3] != '') 
+    {
+    _showWinDialog(elementToDisplay[3]);
+    }
+	if (elementToDisplay[6] == elementToDisplay[7] &&
+		elementToDisplay[6] == elementToDisplay[8] &&
+		elementToDisplay[6] != '') 
+    {
+    _showWinDialog(elementToDisplay[6]);
+    }
+
+	// Checking Column
+	if (elementToDisplay[0] == elementToDisplay[3] &&
+		elementToDisplay[0] == elementToDisplay[6] &&
+		elementToDisplay[0] != '') 
+    {
+    _showWinDialog(elementToDisplay[0]);
+    }
+	if (elementToDisplay[1] == elementToDisplay[4] &&
+		elementToDisplay[1] == elementToDisplay[7] &&
+		elementToDisplay[1] != '') 
+    {
+    _showWinDialog(elementToDisplay[1]);
+    }
+	if (elementToDisplay[2] == elementToDisplay[5] &&
+		elementToDisplay[2] == elementToDisplay[8] &&
+		elementToDisplay[2] != '') 
+    {
+    _showWinDialog(elementToDisplay[2]);
+    }
+
+	// Checking Diagonal
+	if (elementToDisplay[0] == elementToDisplay[4] &&
+		elementToDisplay[0] == elementToDisplay[8] &&
+		elementToDisplay[0] != '') 
+    {
+    _showWinDialog(elementToDisplay[0]);
+    }
+	if (elementToDisplay[2] == elementToDisplay[4] &&
+		elementToDisplay[2] == elementToDisplay[6] &&
+		elementToDisplay[2] != '') 
+    {
+	  _showWinDialog(elementToDisplay[2]);
+	  } 
+  else if (usedBoxes == 9) 
+    {
+    _showDrawDialog();
+    }
+}
+
+void _showWinDialog(String winner) {
+	showDialog(
+		barrierDismissible: false,
+		context: context,
+		builder: (BuildContext context) {
+		return AlertDialog(
+			title: Text("\" " + winner + " \" is Winner!!!"),
+			actions: [
+			OutlinedButton(
+				child: Text("Play Again"),
+				onPressed: () {
+				_clearBoard();
+				Navigator.of(context).pop();
+				},
+			)
+			],
+		);
+		});
+
+	if (winner == 'O') {
+	oPoints++;
+	} else if (winner == 'X') {
+	xPoints++;
+	}
+}
+
+void _showDrawDialog() {
+	showDialog(
+		barrierDismissible: false,
+		context: context,
+		builder: (BuildContext context) {
+		return AlertDialog(
+			title: Text("Draw"),
+			actions: [
+			OutlinedButton(
+				child: Text("Play Again"),
+				onPressed: () {
+				_clearBoard();
+				Navigator.of(context).pop();
+				},
+			)
+			],
+		);
+		});
+}
+
+void _clearBoard() {
+	setState(() {
+	for (int i = 0; i < 9; i++) {
+		elementToDisplay[i] = '';
+	}
+	});
+
+	usedBoxes = 0;
+}
+
+void _clearScoreBoard() {
+	setState(() {
+	xPoints = 0;
+	oPoints = 0;
+	for (int i = 0; i < 9; i++) {
+		elementToDisplay[i] = '';
+	}
+	});
+	usedBoxes = 0;
+}
 }
